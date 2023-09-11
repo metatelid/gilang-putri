aea4a72644fdda045b558f065785fc92b5d31e67
metatelid <88137714+metatelid@users.noreply.github.com>
2023-09-11 23:29:50 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,178 @@
+(function ($) {
+    "use strict";
+
+    // Navbar on scrolling
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.navbar').fadeIn('slow').css('display', 'flex');
+        } else {
+            $('.navbar').fadeOut('slow').css('display', 'none');
+        }
+    });
+
+
+    // Smooth scrolling on the navbar links
+    $(".navbar-nav a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 45
+            }, 1500, 'easeInOutExpo');
+            
+            if ($(this).parents('.navbar-nav').length) {
+                $('.navbar-nav .active').removeClass('active');
+                $(this).closest('a').addClass('active');
+            }
+        }
+    });
+
+
+    // Modal Video
+    $(document).ready(function () {
+        var $videoSrc;
+        $('.btn-play').click(function () {
+            $videoSrc = $(this).data("src");
+        });
+        console.log($videoSrc);
+
+        $('#videoModal').on('shown.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+        })
+
+        $('#videoModal').on('hide.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc);
+        })
+    });
+
+
+    // Scroll to Bottom
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scroll-to-bottom').fadeOut('slow');
+        } else {
+            $('.scroll-to-bottom').fadeIn('slow');
+        }
+    });
+
+
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Gallery carousel
+    $(".gallery-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1500,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            992:{
+                items:4
+            },
+            1200:{
+                items:5
+            }
+        }
+    });
+    
+})(jQuery);
+
+// Waktu akhir countdown
+var countDownDate = new Date("Oct 01, 2023 08:00:00").getTime();
+
+// Update countdown setiap 1 detik
+var x = setInterval(function() {
+
+  // Get tanggal dan waktu saat ini
+  var now = new Date().getTime();
+
+  // Hitung jarak waktu antara sekarang dan waktu akhir countdown
+  var distance = countDownDate - now;
+
+  // Hitung waktu yang tersisa dalam hari, jam, menit, dan detik
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Tampilkan waktu yang tersisa dalam elemen dengan id "countdown"
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  // Jika waktu countdown sudah berakhir, tampilkan pesan
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "Countdown Telah Berakhir";
+  }
+}, 1000);
+
+$(document).ready(function() {
+    $('.btn-modal').trigger('click');
+});
+
+const play = document.querySelector('.play')
+const pause = document.querySelector('.pause')
+
+var el = document.getElementById("music");
+function playAudio() { 
+  el.play();
+  pause.classList.remove('d-none');
+  play.classList.add('d-none');
+} 
+function pauseAudio() { 
+  el.pause();
+  pause.classList.add('d-none');
+  play.classList.remove('d-none');
+}
+
+function copy_bca() {
+    document.getElementById("bca").select();
+    document.execCommand("copy");
+    alert("No. Rekening Berhasil di Copy, Kami tunggu tranferannya :)");
+}
+
+function copy_bri() {
+    document.getElementById("bri").select();
+    document.execCommand("copy");
+    alert("No. Rekening Berhasil di Copy, Kami tunggu tranferannya :)");
+}
